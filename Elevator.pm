056a9b9969e06ad6afa2c3d502d135abce5521f1
amarchenko <titan.vl@gmail.com>
2010-03-31 01:24:52 +0400

    1st rev. Elevator gets you from a floor and then moves to another.

--- Elevator.pm
+++ Elevator.pm
@@ -6,8 +6,9 @@ use warnings;
 use strict;
 
 use constant TOP_FLOOR => 9;
-use constant FREE => 1;
-use constant BUSY => 0;
+use constant ST_FREE => 2;
+use constant ST_QUEUED => 1;
+use constant ST_BUSY => 0;
 use constant UP => 1;
 use constant DOWN => 0;
 
@@ -16,10 +17,11 @@ sub new
    my $class = shift;
    my $self = {};
    bless($self, $class);
-   $self->{state} = FREE;
+   $self->{state} = ST_FREE;
    $self->{direct} = undef;
    $self->{current_floor} = 1;
    $self->{destination_floor} = 0;
+   return $self;
 }
 
 sub get_current_floor
@@ -31,23 +33,49 @@ sub get_current_floor
 sub call_from_floor
 {
     my ($self, $f) = @_;
-    if ($self->{state} == FREE && _is_valid_floor($f))
+    if ($self->{state} == ST_FREE && $self->_is_valid_floor($f))
     {
-        $self->_move_to_floor($f);
+        $self->{destination_floor} = $f;
+        $self->{state} = ST_QUEUED;
+        $self->_move_to_destination(); # is it propper place fro move?
     }
 }
 
 sub set_destination_floor
 {
     my ($self, $f) = @_;
-    if (_is_valid_floor($f))
-        { $self->{destination_floor} = $f; }
+    if ($self->{state} == ST_QUEUED && $self->_is_valid_floor($f))
+    { 
+        $self->{destination_floor} = $f;
+        $self->{state} = ST_BUSY;
+        $self->_move_to_destination(); # is it propper place fro move?
+    }
 }
 
 sub _is_valid_floor
 {
     my ($self, $f) = @_;
     ($f < TOP_FLOOR) && ($f >= 1) ? return 1 : return 0;
+}
+
+sub _move_to_destination
+{
+    my $self = shift;
+    print "Moving: $self->{current_floor} -> $self->{destination_floor}\n";
+    while ($self->{current_floor} < $self->{destination_floor})
+    {
+        $self->{current_floor}++;
+        print "Now at: $self->{current_floor}\n";
+        sleep 1;
+    }
+    while ($self->{destination_floor} < $self->{current_floor})
+    {
+        $self->{current_floor}--;
+        print "Now at: $self->{current_floor}\n";
+        sleep 1;
+    }
+    if ($self->{destination_floor} == $self->{current_floor} && $self->{state} == ST_BUSY)
+        { $self->{state} == ST_FREE; }
 }
 
 1;
--- main.pl
+++ main.pl
@@ -2,4 +2,16 @@
 
 use Elevator;
 
-print "Hura!\n";
+$e = Elevator->new();
+
+print "Where are you? -> ";
+$my_f = <STDIN>;
+chomp($my_f);
+
+$e->call_from_floor($my_f);
+
+print "Where are we going? -> ";
+$my_f = <STDIN>;
+chomp($my_f);
+
+$e->set_destination_floor($my_f);
